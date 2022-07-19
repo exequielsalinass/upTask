3c0c2116c02c7dee8137e1a9fdf3e8800f446b24
Exequiel <e.x.e_salinas3@outlook.com>
2022-07-19 19:09:27 -0300

    Mostrar y eliminar colaboradores

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -25,7 +25,9 @@ const nuevoProyecto = async (req, res) => {
 const obtenerProyecto = async (req, res) => {
   const { id } = req.params;
 
-  const proyecto = await Proyecto.findById(id).populate("tareas");
+  const proyecto = await Proyecto.findById(id)
+    .populate("tareas")
+    .populate("colaboradores", "nombre email");
 
   if (!proyecto) {
     const error = new Error("No Encontrado");
@@ -150,7 +152,24 @@ const agregarColaborador = async (req, res) => {
   res.json({ msg: "Colaborador agregado correctamente" });
 };
 
-const eliminarColaborador = async (req, res) => {};
+const eliminarColaborador = async (req, res) => {
+  const proyecto = await Proyecto.findById(req.params.id);
+
+  if (!proyecto) {
+    const error = new Error("Proyecto No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //Esta ok, se puede eliminar
+  proyecto.colaboradores.pull(req.body.id);
+  await proyecto.save();
+  res.json({ msg: "Colaborador eliminado correctamente" });
+};
 
 export {
   obtenerProyecto,
--- frontend/src/context/ProyectosProvider.jsx
+++ frontend/src/context/ProyectosProvider.jsx
@@ -13,6 +13,7 @@ const ProyectosProvider = ({ children }) => {
   const [tarea, setTarea] = useState({});
   const [modalEliminarTarea, setModalEliminarTarea] = useState(false)
   const [colaborador, setColaborador] = useState({})
+  const [modalEliminarColaborador, setModalEliminarColaborador] = useState(false)
 
   const navigate = useNavigate();
 
@@ -351,6 +352,43 @@ const ProyectosProvider = ({ children }) => {
     }
   }
 
+  const handleModalEliminarColaborador = (colaborador) => {
+    setModalEliminarColaborador(!modalEliminarColaborador)
+
+    setColaborador(colaborador)
+  }
+
+  const eliminarColaborador = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.post(`/proyectos/eliminar-colaborador/${proyecto._id}`, { id: colaborador._id } , config)
+
+      const proyectoActualizado = {...proyecto}
+
+      proyectoActualizado.colaboradores = proyectoActualizado.colaboradores.filter(colaboradorState => colaboradorState._id !== colaborador._id)
+
+      setProyecto(proyectoActualizado)
+      setAlerta({
+        msg: data.msg,
+        error: false
+      })
+      setColaborador({})
+      setModalEliminarColaborador(false)
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
   return (
     <ProyectoContext.Provider
       value={{
@@ -372,7 +410,10 @@ const ProyectosProvider = ({ children }) => {
         eliminarTarea,
         submitColaborador,
         colaborador,
-        agregarColaborador
+        agregarColaborador,
+        handleModalEliminarColaborador,
+        modalEliminarColaborador,
+        eliminarColaborador
       }}
     >
       {children}
